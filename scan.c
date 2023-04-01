f659bd29da4da9afc91e877c98fd4cc425caa7e7
Sayeed1998 <msksayeed1998@gmail.com>
2023-04-02 00:47:18 +0600

    scan.c

---
+++ scan.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    // char h;
+
+    // scanf("%d %c %d %c",&a,&h,&b,&h);
+    // printf("%d%c %d%c",a,h,b,h);
+
+
+    // scanf("%d%% %d%%",&a,&b);
+    // printf("%d%% %d%%",a,b);
+
+    scanf("%da %db",&a,&b);
+    printf("%da %db",a,b); 
+    return 0;
+}
